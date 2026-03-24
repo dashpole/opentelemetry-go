de3dcebe2df5a8033042bab32166a20ca2a329a0
David Ashpole <dashpole@google.com>
2026-03-24 19:54:45 +0000

    add internal interface to indicate experimental options

---
+++ internal/x/options.go
@@ -0,0 +1,10 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package x // import "go.opentelemetry.io/otel/internal/x"
+
+// ExperimentalOption is an interface that can be used to signal to API and SDK
+// configuration that an option is defined in an experimental module.
+type ExperimentalOption interface {
+	experimentalOption()
+}
